51204c0a284116a327390bbd78168522b1680bd7
welingtonfidelis <welingtonfidelis@gmail.com>
2021-10-06 19:02:47 -0300

    1 file modified

--- index.js
+++ index.js
@@ -5,31 +5,33 @@ import './style.css';
 let html = `<h1>Atividade</h1>`;
 const text = "Atividade Empírica";
 
-html += `charAt(posicao) => resultado: ${text.charAt(0)} </br></br>`;
+html += `String usada nos métodos abaixo => ${text} </br></br>`;
 
-html += `charcodeAt(posicao) => resultado: ${text.charCodeAt(0)} </br></br>`;
+html += `charAt(posicao) => Resultado: ${text.charAt(0)} => Resumo do método: Retorna o caractere na posição passada no método. </br></br>`;
 
-html += `indexOf(substring) => resultado: ${text.indexOf("a")} </br></br>`;
+html += `charcodeAt(posicao) => Resultado: ${text.charCodeAt(0)} => Resumo do método: Retorna o número na tabela ASCII para o caracteren a posicao passada no método. </br></br>`;
 
-html += `search(substring) => resultado: ${text.search("a")} </br></br>`;
+html += `indexOf(substring) => Resultado: ${text.indexOf("a")} => Resumo do método: Retorna a posição (a partir de 0) da primeira aparição da string passada como parâmetro. </br></br>`;
 
-html += `lastIndexOf(substring) => resultado: ${text.lastIndexOf("a")} </br></br>`;
+html += `search(substring) => Resultado: ${text.search("a")} => Resumo do método: Retorna a posição (a partir de 0) da primeira aparição da string passada como parâmetro. </br></br>`;
 
-html += `slice(inicio, fim) => resultado: ${text.slice(0, 8)} </br></br>`;
+html += `lastIndexOf(substring) => Resultado: ${text.lastIndexOf("a")} => Resumo do método: Retorna a posição (a partir de 0) da última aparição da string passada como parâmetro. </br></br>`;
 
-html += `substr(inicio, tamanho) => resultado: ${text.substr(0, 6)} </br></br>`;
+html += `slice(inicio, fim) => Resultado: ${text.slice(0, 8)} => Resumo do método: Retorna parte da string partindo da posição inicial (primeiro parâmetro passado) e indo até a posição final (segundo parâmetro passado). </br></br>`;
 
-html += `substring(inicio, fim) => resultado: ${text.substring(0, 6)} </br></br>`;
+html += `substr(inicio, tamanho) => Resultado: ${text.substr(0, 8)} => Resumo do método: Retorna parte da string partindo da posição inicial (primeiro parâmetro passado) e indo até número de caracteres máximo (segundo parâmetro passado). </br></br>`;
 
-html += `replace(substringExistente, novaString) => resultado: ${text.replace("Atividade", "Super Atividade")} </br></br>`;
+html += `substring(inicio, fim) => Resultado: ${text.substring(0, 8)} => Resumo do método: Retorna parte da string partindo da posição inicial (primeiro parâmetro passado) e indo até a posição final (segundo parâmetro passado). </br></br>`;
 
-html += `toUpperCase(substringExistente, novaString) => resultado: ${text.toUpperCase()} </br></br>`;
+html += `replace(substringExistente, novaString) => Resultado: ${text.replace("Atividade", "Super Atividade")} => Resumo do método: Retorna a string com parte substituida (primeiro parâmetro de busca) por outra string (segundo parâmetro). </br></br>`;
 
-html += `toLowerCase(substringExistente, novaString) => resultado: ${text.toLowerCase()} </br></br>`;
+html += `toUpperCase(substringExistente, novaString) => Resultado: ${text.toUpperCase()} => Resumo do método: Retorna a string com TODOS os caracteres em maiúsculo. </br></br>`;
 
-html += `concat(string) => resultado: ${text.concat(" - 1")} </br></br>`;
+html += `toLowerCase(substringExistente, novaString) => Resultado: ${text.toLowerCase()} => Resumo do método: Retorna a string com TODOS os caracteres em minúsculo. </br></br>`;
 
-html += `split(separador) => resultado: ${text.split(" ")} </br></br>`;
+html += `concat(string) => Resultado: ${text.concat(" - 1")} => Resumo do método: Retorna a string original com uma nova string incluída no fim. </br></br>`;
+
+html += `split(separador) => Resultado: ${text.split(" ")} => Resumo do método: Retorna um array de strings, separando a string original a partir do parâmetro passado. </br></br>`;
 
 
 const appDiv = document.getElementById('app');
